06cbe15dcb04581a0876df33d90a13d8799cf12d
Julien Leclercq <julien.leclercq@gmx.fr>
2020-09-10 17:17:45 +0200

    adjust padding and extract genrate

--- main.go
+++ main.go
@@ -16,20 +16,15 @@ func main() {
 	flag.StringVar(&prefix, "prefix", "", "provide a prefix for your error code, it usually is the first 3 letters of the name of the project you're working on")
 	flag.Parse()
 
+	if prefix == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
 	now := time.Now()
+	code := generate(prefix, now)
 
-	year := strconv.Itoa(now.Year() % 100)
-	month := strconv.Itoa(int(now.Month()))
-	day := strconv.Itoa(now.Day())
-	hour := strconv.Itoa(now.Hour())
-	minute := strconv.Itoa(now.Minute())
-	second := strconv.Itoa(now.Second())
-	microsecond := strconv.Itoa(now.Nanosecond() / 1000)
-
-	part1 := base34Encode([]byte(month)) + base34Encode([]byte(day))
-	part2 := fmt.Sprintf("%5v", base34Encode([]byte(hour+minute+second+microsecond)))
-
-	fmt.Printf("%s_%s%s%s", prefix, year, part1, part2)
+	fmt.Printf(code)
 }
 
 func base34Encode(source []byte) string {
@@ -39,4 +34,20 @@ func base34Encode(source []byte) string {
 		os.Exit(1)
 	}
 	return encoder.Encode(source)
+}
+
+func generate(prefix string, now time.Time) string {
+	year := strconv.Itoa(now.Year() % 100)
+	month := strconv.Itoa(int(now.Month()))
+	day := strconv.Itoa(now.Day())
+	hour := strconv.Itoa(now.Hour())
+	minute := strconv.Itoa(now.Minute())
+	second := strconv.Itoa(now.Second())
+	microsecond := strconv.Itoa(now.Nanosecond() / 1000)
+
+	part1 := base34Encode([]byte(month)) + base34Encode([]byte(day))
+
+	part2 := fmt.Sprintf("%6v", base34Encode([]byte(hour+minute+second+microsecond)))
+
+	return fmt.Sprintf("%s_%s%s%s", prefix, year, part1, part2)
 }
